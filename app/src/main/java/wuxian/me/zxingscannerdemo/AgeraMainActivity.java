618c2bf9070b2bbfcba90610c0d3f542453cf4af
xiaoshenke <1205914137@qq.com>
2016-10-18 20:52:08 +0800

    finish agera integration

--- app/src/main/java/wuxian/me/zxingscannerdemo/AgeraMainActivity.java
+++ app/src/main/java/wuxian/me/zxingscannerdemo/AgeraMainActivity.java
@@ -5,19 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
 import android.widget.Toast;
 
+import com.google.android.agera.Updatable;
+
 import rx.functions.Action1;
+import wuxian.me.zxingscanner.agera.QRCodeScannerRepository;
 import wuxian.me.zxingscanner.decoding.InactivityTimer;
 import wuxian.me.zxingscanner.demo.R;
 import wuxian.me.zxingscanner.rx.RxQRCodeScanner;
 import wuxian.me.zxingscanner.view.ScanView;
 
-/**
- * todo: to be finished
- */
-public class AgeraMainActivity extends AppCompatActivity {
+public class AgeraMainActivity extends AppCompatActivity implements Updatable{
 
     private ScanView mScanView;
     private SurfaceView mSurfaceView;
+
+    private QRCodeScannerRepository mRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class AgeraMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
 
+        mRepository = new QRCodeScannerRepository().surfaceView(mSurfaceView).scanView(mScanView);
     }
 
     private void initView() {
@@ -36,11 +39,18 @@ public class AgeraMainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
 
+        mRepository.addUpdatable(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        mRepository.removeUpdatable(this);
     }
 
+    @Override
+    public void update() {
+        Toast.makeText(this,"qrcode is "+mRepository.get(),Toast.LENGTH_LONG).show();
+    }
 }
--- library/src/main/java/wuxian/me/zxingscanner/agera/AgeraRQCodeScanner.java
+++
@@ -1,9 +0,0 @@
-package wuxian.me.zxingscanner.agera;
-
-/**
- * Created by wuxian on 17/10/2016.
- * Todo:to be implemented
- */
-
-public class AgeraRQCodeScanner {
-}
