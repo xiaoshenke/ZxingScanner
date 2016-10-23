9194e15d200e308ca0174f45df01d2e6784fef56
xiaoshenke <1205914137@qq.com>
2016-10-23 22:38:09 +0800

    rxjava version design

--- library/src/main/java/wuxian/me/zxingscanner/rxversion/QRCodeObservable.java
+++ library/src/main/java/wuxian/me/zxingscanner/rxversion/QRCodeObservable.java
@@ -7,6 +7,7 @@ import rx.Subscriber;
  * Created by wuxian on 23/10/2016.
  * <p>
  * In this way QRCodeObservable is not a Observable,it only has a Observable interface....
+ * Fixme: a better way ? 比如说init camera 在@OnSubscribeFromCamera 类中
  * <p>
  * Todo: to be finished
  */
